8c0b28f9aaf97c9bc81e824dcfa16c1def96baab
Tfontaine <tom_1993@hotmail.fr>
2015-07-28 01:35:27 +0200

    - Final push

--- GarbageCollector/Assets/Thomas/Scripts/ShipMovement.cs
+++ GarbageCollector/Assets/Thomas/Scripts/ShipMovement.cs
@@ -14,6 +14,12 @@ public class ShipMovement : MonoBehaviour {
 	[HideInInspector]
 	public bool shipMoving = true;
 
+	[HideInInspector]
+	public bool inverseYAxis = true;
+
+	[HideInInspector]
+	public bool inverseXAxis = true;
+
 	private Transform shipTransform;
 
 	private float initialSpeed;
@@ -22,10 +28,37 @@ public class ShipMovement : MonoBehaviour {
 		initialSpeed = speed;
 	}
 
+	public void Start() {
+		if(PlayerPrefs.GetString("inverseXAxis") == "True") {
+			inverseXAxis = true;
+		} else {
+			inverseXAxis = false;
+		}
+
+		if(PlayerPrefs.GetString("inverseYAxis") == "True") {
+			inverseYAxis = true;
+		} else {
+			inverseYAxis = false;
+		}
+
+		Debug.Log(inverseXAxis);
+	}
+
 	void Update () {
 		if(shipMoving) {
 			Moving();
 		}
+	}
+
+	public void ChangeXAxis(bool inverse) {
+		inverseXAxis = inverse;
+		PlayerPrefs.SetString("inverseXAxis", inverse.ToString());
+
+	}
+
+	public void ChangeYAxis(bool inverse) {
+		inverseYAxis = inverse;
+		PlayerPrefs.SetString("inverseYAxis", inverse.ToString());
 	}
 
 	private void Moving() {
@@ -35,12 +68,25 @@ public class ShipMovement : MonoBehaviour {
 		else  {
 			speed -= Time.deltaTime * 10;
 		}
-		
+
+		float inputVertical = MovementJoystick.GetAxis("Vertical");
+		float inputHorizontal = MovementJoystick.GetAxis("Horizontal");
+
+
+		if(inverseXAxis) {
+			inputVertical = -MovementJoystick.GetAxis("Vertical");
+		}
+
+		if(inverseYAxis) {
+			inputHorizontal = -MovementJoystick.GetAxis("Horizontal");
+		}
+
 		speed = Mathf.Clamp(speed, initialSpeed, maxSpeed);
-		
+
+
 		transform.Rotate(
-			MovementJoystick.GetAxis("Vertical") * Time.deltaTime * speedRotation,
-			MovementJoystick.GetAxis("Horizontal") * Time.deltaTime * speedRotation,
+			inputVertical * Time.deltaTime * speedRotation,
+			inputHorizontal * Time.deltaTime * speedRotation,
 			0);
 		
 		
